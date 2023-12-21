2f5975e2fcbfd5689cf67c279631540adc7df4ff
Nursandi <sandinur157@gmail.com>
2023-12-21 14:10:41 +0700

    finish chapter 15

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <Link href="/dashboard" className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <Link href="/" className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </Link>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
